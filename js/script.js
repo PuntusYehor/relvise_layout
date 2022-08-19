c2d1d7d618a3f6dad63e69f5c46d824623e64aa3
PuntusYehor <maingithubacc@gmail.com>
2022-08-19 22:06:15 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// "use strict"
+
+// let eample = prompt("Введите ваше имя пользователя", []);
+
+// console.log(eample);
+
+// let userName = confirm(`Ваше имя пользователя ${eample}?`);
+// console.log(userName);
+
+// if (userName === true) {
+// 	alert(`Правильно! Ваше имя пользователя ${eample}`);
+// } else {
+// 	alert(`Вы ввели неправильное имя пользователя`);
+// }
+
+
